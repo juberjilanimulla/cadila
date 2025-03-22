32e7754559e5d9468c11530ff296e105265a882a
Juber <juber.mulla@suyotechsolutions.com>
2025-03-22 12:12:39 +0530

    first commit

--- routes/admin/adminRouter.js
+++ routes/admin/adminRouter.js
@@ -3,6 +3,7 @@ import admincontactRouter from "./admincontactusRouter.js";
 import adminDashboardRouter from "./adminDashboardRouter.js";
 import adminjobpostingRouter from "./adminjobpostingRouter.js";
 import admintalenthireRouter from "./admintalenthireRouter.js";
+import adminjobapplicantsRouter from "./adminjobapplicantsRouter.js";
 
 const adminRouter = Router();
 
@@ -10,6 +11,5 @@ adminRouter.use("/contactus", admincontactRouter);
 adminRouter.use("/dashboard", adminDashboardRouter);
 adminRouter.use("/jobposting", adminjobpostingRouter);
 adminRouter.use("/hiretalent", admintalenthireRouter);
-
+adminRouter.use("/jobapplicants", adminjobapplicantsRouter);
 export default adminRouter;
-
